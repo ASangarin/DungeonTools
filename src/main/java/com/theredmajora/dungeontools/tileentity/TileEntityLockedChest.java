247a17d9138f2f8d1a6e80c12c2d618c5075b324
ASangarin <ariasangarin@gmail.com>
2020-08-01 01:52:29 +0200

    updates

--- src/main/java/com/theredmajora/dungeontools/tileentity/TileEntityLockedChest.java
+++ src/main/java/com/theredmajora/dungeontools/tileentity/TileEntityLockedChest.java
@@ -129,7 +129,7 @@ public class TileEntityLockedChest extends TileEntityLockableLoot implements ITi
             double d1 = (double)i + 0.5D;
             double d2 = (double)k + 0.5D;
 
-            this.world.playSound((EntityPlayer)null, d1, (double)j + 0.5D, d2, SoundEvents.BLOCK_CHEST_OPEN, SoundCategory.BLOCKS, 0.5F, this.world.rand.nextFloat() * 0.1F + 0.9F);
+            this.world.playSound(null, d1, (double)j + 0.5D, d2, SoundEvents.BLOCK_CHEST_OPEN, SoundCategory.BLOCKS, 0.5F, this.world.rand.nextFloat() * 0.1F + 0.9F);
         }
 
         if (this.numPlayersUsing == 0 && this.lidAngle > 0.0F || this.numPlayersUsing > 0 && this.lidAngle < 1.0F)
